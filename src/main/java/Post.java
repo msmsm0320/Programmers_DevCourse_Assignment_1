5c50dbe2b2388dcc903d2cec2e56d56de7a7d40e
msmsm0320 <ms000320@naver.com>
2025-03-17 19:02:58 +0900

    refactoring file

---
+++ src/main/java/Post.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+
+    private static long countId = 0L;
+    private long id;
+    private String title;
+    private String content;
+
+    public Post(String title, String content) {
+        id = countId++;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
